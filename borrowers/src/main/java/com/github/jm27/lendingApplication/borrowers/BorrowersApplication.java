07d28824d22c61597374588d19519490bb8a2d50
jm27 <jme2791@gmail.com>
2021-08-24 20:42:43 -0700

    Created starter files for borrower

---
+++ borrowers/src/main/java/com/github/jm27/lendingApplication/borrowers/BorrowersApplication.java
@@ -0,0 +1,13 @@
+package com.github.jm27.lendingApplication.borrowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowersApplication.class, args);
+	}
+
+}
