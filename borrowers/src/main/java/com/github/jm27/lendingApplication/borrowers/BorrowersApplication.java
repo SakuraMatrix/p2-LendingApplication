84e7360b959237327b8b8006ad4f4ad02e1173cb
jm27 <jme2791@gmail.com>
2021-08-26 20:04:23 -0700

    discovery and gateway working

--- borrowers/src/main/java/com/github/jm27/lendingApplication/borrowers/BorrowersApplication.java
+++ borrowers/src/main/java/com/github/jm27/lendingApplication/borrowers/BorrowersApplication.java
@@ -2,8 +2,10 @@ package com.github.jm27.lendingApplication.borrowers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BorrowersApplication {
 
 	public static void main(String[] args) {
